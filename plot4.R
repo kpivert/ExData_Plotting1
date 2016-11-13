b02d0249a405edbb2595fba2d07d516ebf3a60b8
Kurtis Pivert <kurtis.pivert@gmail.com>
2016-11-13 14:41:47 -0500

    Added Plot 4 to Repo

---
+++ plot4.R
@@ -0,0 +1,90 @@
+
+## R Code to create Plot 4: Matrix of 4 plots: 1) Global Active Power against minute; 2)  Voltage against minute; 3) Energy sub metering against minute; 4)
+## showing Plot of Energy Submetering by minute for 2007-02-01 and 2007-02-02
+
+
+## Download Data File From Coursera Website and Unzip Contents
+
+
+          temp <- tempfile()
+          download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", 
+                        temp)
+          con <- unz(temp, "household_power_consumption.txt")
+          dat <- read.table(con, header=T, sep=";", na.strings = "?")
+          unlink(temp)
+
+## Subset Data for Observations from 2007-02-01 and 2007-02-02
+
+          target <- c("2/1/2007", "2/2/2007")
+          febpower <- subset(dat, Date %in% target)
+
+
+## Change Date and Time 
+
+          febpower$datetime <- paste(febpower$Date, febpower$Time)
+          febpower$datetime <- strptime(febpower$datetime, "%m/%d/%Y %H:%M:%S")
+
+
+
+## Create matrix of plots 
+          par(mfrow= c(2,2), mar=c(5.1,4.1,4.1,2.1))
+          
+## Plot 1: Global Active Power against minute           
+          
+          with(febpower,  plot(datetime, Global_active_power, ylab = "Global Active Power",
+                               type = "l", ylim = c(0,8)), xlab="")
+          
+## Plot 2: Voltage against minute
+          with(febpower,  plot(datetime, Voltage, ylab = "Voltage",
+                               type = "l", ylim = c(232,250)), xlab="")        
+
+          
+## Plot 3: Energy sub metering against minute
+          with(febpower,  plot(datetime, Global_active_power, ylab = "Energy Sub metering",
+                               type = "n", ylim = c(0,40)), xlab="")
+          with(febpower, lines(datetime, Sub_metering_1, col="black")) 
+          with(febpower, lines(datetime, Sub_metering_2, col="red"))
+          with(febpower, lines(datetime, Sub_metering_3, col="blue"))
+          legend("topright", col=c("black", "red", "blue"), lwd=1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+          
+## Plot 4:   
+          with(febpower,  plot(datetime, Global_reactive_power, ylab = "Global_reactive_power",
+                               type = "l", ylim = c(0.0,0.9)), xlab="")   
+
+          
+          
+## Save Plot 4 as PNG
+
+
+
+          png("plot4.png", width = 480, height = 480)
+          
+          ## Create matrix of plots 
+          par(mfrow= c(2,2), mar=c(3,4.1,4.1,2.1))
+          
+          ## Plot 1: Global Active Power against minute           
+          
+          with(febpower,  plot(datetime, Global_active_power, ylab = "Global Active Power",
+                               type = "l", ylim = c(0,8)), xlab="")
+          
+          ## Plot 2: Voltage against minute
+          with(febpower,  plot(datetime, Voltage, ylab = "Voltage",
+                               type = "l", ylim = c(232,250)), xlab="")        
+          
+          
+          ## Plot 3: Energy sub metering against minute
+          with(febpower,  plot(datetime, Global_active_power, ylab = "Energy Sub metering",
+                               type = "n", ylim = c(0,40)), xlab="")
+          with(febpower, lines(datetime, Sub_metering_1, col="black")) 
+          with(febpower, lines(datetime, Sub_metering_2, col="red"))
+          with(febpower, lines(datetime, Sub_metering_3, col="blue"))
+          legend("topright", col=c("black", "red", "blue"), lwd=1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+          
+          ## Plot 4:   
+          with(febpower,  plot(datetime, Global_reactive_power, ylab = "Global_reactive_power",
+                               type = "l", ylim = c(0.0,0.9)), xlab="") 
+              
+          dev.off()
+
+
+
